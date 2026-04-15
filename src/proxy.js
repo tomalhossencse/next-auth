58c8d5c99dfdbf75bb4f52c2a1767041db3d6fd0
Tomal Hossen <tomalhossen78@gmail.com>
2026-04-15 15:03:18 +0600

    feat : add adminRoute Functinality

--- src/proxy.js
+++ src/proxy.js
@@ -3,19 +3,37 @@ import { NextResponse } from "next/server";
 
 const privateRoutes = ["/private", "/dashboard", "/secreet"];
 
+const adminRoutes = ["/dashboard"];
+
 // This function can be marked `async` if using `await` inside
 export async function proxy(req) {
   const token = await getToken({ req });
+
   const reqPath = req.nextUrl.pathname;
+
   const isLogin = Boolean(token);
+
   const isUser = token?.role === "user";
+
+  const isAdmin = token?.role === "admin";
+
   const isPrivate = privateRoutes.some((route) => reqPath.startsWith(route));
   console.log({ isLogin, isUser, reqPath, isPrivate });
 
+  const isAdminRoute = adminRoutes.some((route) => reqPath.startsWith(route));
+
+  // logic for private route only
   if (!isLogin && isPrivate) {
     const loginUrl = new URL("/api/auth/signin", req.url);
+
     loginUrl.searchParams.set("callbackUrl", reqPath);
+
     return NextResponse.redirect(loginUrl);
+  }
+
+  // logic for admin route only
+  if (isLogin && !isAdmin && isAdminRoute) {
+    return NextResponse.redirect(new URL("/forbidden", req.url));
   }
 
   //   return NextResponse.redirect(new URL("/home", req.url));
